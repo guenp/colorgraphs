b9f1832a3d2633771eebed4a684a85f6495b0e35
Guen P <guen@nbi.dk>
2016-01-27 11:11:46 -0500

    make package

--- setup.py
+++ setup.py
@@ -1,12 +1,18 @@
-from setuptools import setup
+from distutils.core import setup
 
 setup(
 	name="Colorgraphs",
 	version="1.0",
 	author="Guen P",
 	author_email="guen@nbi.dk",
+    packages=[
+        'colorgraphs'
+    ],
+    license='LICENSE',
+    description='Pyqtgraph tools for ipython notebook',
+    long_description=open('README.md').read(),
 	install_requires=[
-		"pyqtgraph",
-		"configparser"
+        "numpy",
+		"pyqtgraph"
 	],
 )
